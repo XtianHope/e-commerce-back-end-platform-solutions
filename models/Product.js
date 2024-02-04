c786bf816b62a92e7c5382cb0cc0036f3ba89683
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-02-04 18:39:35 -0500

    added id and category id values

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,12 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
@@ -28,6 +34,13 @@ Product.init(
         isNumeric: true
       }
     },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
