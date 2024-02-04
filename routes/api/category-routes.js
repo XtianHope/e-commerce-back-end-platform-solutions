da3391135c83483f6d9f57beba3e9cf14a34b3d6
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-02-03 20:10:07 -0500

    added try for finding one category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     // be sure to include its associated Products
     try {
       const categoryData = await Category.findAll({
-        include: [{ model: Product }],
+        include: Product,
       });
       res.status(200).json(categoryData);
     } catch (err) {
@@ -19,6 +19,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
