5e7304c82c1c7e51924c932cad8b2c377ed67131
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-02-03 21:09:49 -0500

    resolved duplicate PORT variable issue

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
