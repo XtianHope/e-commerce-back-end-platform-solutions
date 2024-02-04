19862b28903585321daedd145f9319b8ce0c0876
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-02-03 19:50:15 -0500

    updated category.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 const PORT = process.env.PORT || 3001;
 
